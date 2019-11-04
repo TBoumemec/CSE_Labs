37632381902c52c03afbb6f1e6fbe8c0672df518
tBoumemec <zhalig@inbox.ru>
2019-11-05 00:30:48 +0300

    Lab1 v 1.0.1 - some slight editings Lab2 v 0.1 - body of program

--- src/Lab2/Test.py
+++ src/Lab2/Test.py
@@ -6,22 +6,22 @@ from src.Lab2 import toolBox
 from src.Lab2 import initPamsLab2
 
 
-w = initPamsLab2.finish_chain()
-print(w)
+# w = initPamsLab2.finish_chain()
+# print(w)
 
 
 b = int(input("Input your desired analyze tool?\n 1 - transition function \n 2 - bode function \n"
-                  " 3 - Nyquist diagram \n 4 - Gurwitz criterion \n" " : "));
+                  " 3 - Nyquist diagram \n 4 - Gurwitz criterion \n" " : "))
 
 dictionary_of_choice = {
     1: toolBox.trans_func,
     2: toolBox.bode_func,
     3: toolBox.nyquist_diagram,
     4: toolBox.gurwitz_crit,
-};
+}
 
 try:
-    f = dictionary_of_choice[b](w)
+    f = dictionary_of_choice[b]()
 except KeyError as e:
     raise ValueError('Undefined unit: {}'.format(e.args[0]))
 
--- src/Lab2/toolBox.py
+++ src/Lab2/toolBox.py
@@ -1,13 +1,18 @@
 from control import *
 import matplotlib.pyplot as plt
 from control.matlab import step
-from sympy import det
+from numpy.linalg import det
+from src.Lab2 import initPamsLab2
 
 t = np.linspace(0, stop=50, num=1000)
 
 
 
-def trans_func(w):
+def trans_func():
+
+    print(3)
+    w = initPamsLab2.calc_w()
+
     print(w)
     y1, t1 = step(w, t)
     plt.plot(t, y1, "r")
@@ -20,7 +25,9 @@ def trans_func(w):
     return
 
 
-def bode_func(w):
+def bode_func():
+
+    w = initPamsLab2.calc_w()
 
     print(w)
     mag, phase, omega = bode(w, dB=False)
@@ -31,7 +38,10 @@ def bode_func(w):
     return
 
 
-def nyquist_diagram(w):
+def nyquist_diagram():
+
+    w = initPamsLab2.calc_w()
+
     real, imag, freq = nyquist(w, labelFreq=10, color='g')
     plt.title('Nyquist Diagram')
     plt.plot()
@@ -39,10 +49,11 @@ def nyquist_diagram(w):
     #
     return
 
-def poles_analaze(w):
+def poles_analaze():
 
     from control import pole
 
+    w = initPamsLab2.calc_w()
     poles = pole(w)
     print(poles)
     pole, zeros = pzmap(w)
@@ -51,28 +62,37 @@ def poles_analaze(w):
     plt.show()
 
 
-def gurwitz_crit(w):
+def gurwitz_crit():
+
+    w = initPamsLab2.calc_w()
+
     num, denum = tfdata(w)
-    num = num[0][0][:]
+    # num = num[0][0][:]
     denum = denum[0][0][:]
 
-    matrix = np.zeros((len(denum) - 1, len(denum) - 1))
+    matrix = np.zeros((len(denum)-1, len(denum)-1))
     a1 = 0
 
     for i in range(len(denum) - 1):
-        if (i + 1) % 2 != 0:
+        if i % 2 == 0:
             a = int((i + 1) // 2)
+
             a1 = a
 
         else:
             a = a1
 
-        for j in range(len(denum) - 1):
-            if (j % 2) == (i % 2):
-                matrix[i][a] = denum[j]
+        for j in range(len(denum)):
+
+            if (j % 2) != (i % 2):
+                matrix[i, a] = denum[j]
+                # print(a)
+                # print(matrix)
                 a += 1
 
-    for i in range((len(matrix)), -1, -1):
+    print(matrix)
+
+    for i in range(len(matrix), 0, -1):
         opr = det(matrix[:i, :i])
         print(opr)
         if opr < 0:
