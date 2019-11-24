be4391718e7bba1996a443ea961aa13396c275d2
tBoumemec <zhalig@inbox.ru>
2019-11-24 16:25:31 +0300

    version 2.0

--- src/Lab3/Function_analyzing_tools.py
+++ src/Lab3/Function_analyzing_tools.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from src.Lab3.Initial_parameters import Initialazer
 
-
 """
 Class let work with transition function(get_trans_func), analyzing of poles(get_poles_analyze),
                     godoghraph plotting(get_godoghraph)
@@ -18,7 +17,7 @@ Class let work with transition function(get_trans_func), analyzing of poles(get_
 
 class My_function:
 
-    def __init__(self, w_f= Initialazer().get_scheme_solving()):
+    def __init__(self, w_f=Initialazer().get_scheme_solving()):
         self.w = w_f
         self.t = np.linspace(0, stop=50, num=1000)
 
@@ -34,20 +33,36 @@ class My_function:
         last = y1[-1]
 
         perereg = (maxis - last) / last * 100
-        print("Перерегулирование составляет: " + str(perereg) + " %")
+        print("************************\n"
+              "Перерегулирование составляет: " + str(perereg) + " %")
+
+        if perereg > 30:
+            print("Полученная величина выше оптимальной области регулирования")
+        elif perereg <10:
+            print("Полученная величина ниже оптимальной области регулирования")
+        else:
+            print("Отлично")
 
         y2 = list(y1)
         y3 = list(y1)
 
-        print("Величина: " + str(max(y2)) + " и время достижения первого максимума: "
+        print("************************\n"
+              "Величина: " + str(max(y2)) + " и время достижения первого максимума: "
               + str(self.t[y2.index(max(y2))]))
 
         del y2[0:y2.index(max(y2))]
         del y2[0:y2.index(min(y2))]
 
         step_zat = 1 - max(y2) / max(y1)
-        print("Степень затухания составляет: ", step_zat)
+        print("************************\n"
+              "Степень затухания составляет: ", step_zat)
 
+        if step_zat > 0.95:
+            print("Полученная величина выше оптимальной области регулирования")
+        elif step_zat < 0.75:
+            print("Полученная величина ниже оптимальной области регулирования")
+        else:
+            print("Отлично")
 
         numnum = 0
 
@@ -56,7 +71,8 @@ class My_function:
                 # num = i
                 numnum += 1
                 if numnum == 20:
-                    print("Время регулирования: " + str(self.t[i]) + " c")
+                    print("************************\n"
+                          "Время регулирования: " + str(self.t[i]) + " c")
                     break
             else:
                 numnum = 0
@@ -67,13 +83,21 @@ class My_function:
             if max(y3) > 1.05 * last:
                 del y3[0:y3.index(max(y3))]
                 koleb += 1
-            else : break
+            else:
+                break
             if 0.95 * last > min(y3):
                 del y3[0:y3.index(min(y3))]
-            else: break
+            else:
+                break
 
-        print("Колебательность составляет: ", koleb, " вершин,"
-                                     " или ", max(y2)/max(y1)*100, "%" )
+        print("************************\n"
+              "Колебательность составляет: ", koleb, " вершин"
+                                                     " или ", max(y2) / max(y1) * 100, "%")
+
+        if koleb > 2:
+            print("Полученная величина колебаний выше оптимальной области регулирования")
+        else:
+            print("Отлично")
 
         # *****************************
         # print("Интегральчик: " + str(integrate.quad(y1)))
@@ -96,6 +120,7 @@ class My_function:
 
         pole, zeros = pzmap(self.w)
 
+        a = True
         print("Полюса плоскости: ")
         for i in range(len(pole)):
             if pole[i].real >= 0:
@@ -119,7 +144,6 @@ class My_function:
         print("Перерегулирование: " + str(e + (pi / degree_max)))
         print("Степень затухания: " + str(1 - e - (2 * pi / degree_max)))
 
-
         plt.title('Graph of poles')
         plt.plot()
         plt.show()
@@ -129,6 +153,22 @@ class My_function:
         mag, phase, omega = bode(self.w, dB=False)
 
         print("Показатель колебательности M: " + str(max(mag) / mag[0]))
+
+        c = []
+
+        for i in range(1, len(mag)):
+            if mag[0] - 0.1 < mag[i] < mag[0] + 0.1:
+                c.append(i)
+
+        # print("Частота среза: ", str(omega[c[-1]]))
+
+        print("Время регулирования: ", str(2 * 2*pi/omega[c[-1]]))
+
+        plt.title("Frequency Response", y=2.2)
+        plt.plot()
+        plt.show()
+
+        mag, phase, omega = bode(self.w, dB=True)
 
         a = []
         b = []
@@ -150,14 +190,13 @@ class My_function:
 
     def full_analyze(self):
         print("\n****************************************\n"
-              "Step Responce:\n")
+              "STEP RESPONCE:\n")
         My_function().get_trans_func()
 
         print("\n****************************************\n"
-              "Poles analyze:\n")
+              "POLES ANALYZE:\n")
         My_function().get_poles_analyze()
 
         print("\n****************************************\n"
-              "Bode function:\n")
+              "BODE FUNCTION:\n")
         My_function().get_bode_func()
-
